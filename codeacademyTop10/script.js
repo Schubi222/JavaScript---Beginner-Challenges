09eae28c4527f5d6bbdc4e93d78789e4f5b46ab0
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2023-05-18 14:37:59 +0200

    a few more Challenges

--- codeacademyTop10/script.js
+++ codeacademyTop10/script.js
@@ -1,7 +1,26 @@
 function printEven(maxNum){
     for (let i = 0; i <= maxNum; i++) {
-        i%2 === 0 ? console.log(i) : null
+        i%2 === 0 ? console.log("printEven: ",i) : null
     }
 }
 
-printEven(100)
+function multiplicationTable(numTimes){
+    for (let i = 1; i <= 10; i++) {
+        console.log("multiplicationTable: ", numTimes*i)
+    }
+}
+
+function KMtoMiles(KM){
+    return KM*1.609
+}
+
+function sumArray(array){
+    let sum = 0.0
+    array.forEach(e => sum += e)
+    return sum
+}
+
+printEven(10)
+multiplicationTable(12)
+console.log("KMtoMiles", KMtoMiles(10))
+console.log("sumArray", sumArray([10,-1,0.5,3]))
