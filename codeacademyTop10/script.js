4d9a94079a21a87fb609e7ed4123b75c2a81720a
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2023-05-18 17:51:46 +0200

    More Challenges

--- codeacademyTop10/script.js
+++ codeacademyTop10/script.js
@@ -1,3 +1,4 @@
+
 function printEven(maxNum){
     for (let i = 0; i <= maxNum; i++) {
         i%2 === 0 ? console.log("printEven: ",i) : null
@@ -20,7 +21,29 @@ function sumArray(array){
     return sum
 }
 
+function reverseArray(array){
+    let newArray = []
+    for (let i = array.length-1; i >= 0; i--) {
+        newArray.push(array[i])
+    }
+    return newArray
+}
+
+function reverseArrayInPlace(array){
+    const length =  array.length-1
+    for (let i = length; i >= Math.ceil(length/2); i--) {
+        const backNumber = array[i]
+        const frontNumber = array[length-i]
+        array[length-i] = backNumber
+        array[i] = frontNumber
+
+    }
+    return array
+}
+
 printEven(10)
 multiplicationTable(12)
-console.log("KMtoMiles", KMtoMiles(10))
-console.log("sumArray", sumArray([10,-1,0.5,3]))
+console.log("KMtoMiles: ", KMtoMiles(10))
+console.log("sumArray: ", sumArray([10,-1,0.5,3]))
+console.log("reverseArray: ", reverseArray([1,2,3,4,5,6,7,8,9,10]))
+console.log("reverseArrayInPlace: ", reverseArrayInPlace([1,2,3,4,5,6,7,8,9,10,11]))
